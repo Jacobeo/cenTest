df73eb4668229a1f05edbee1906958e4499b9fb9
Jacob Egholm Ottesen <jacob.e.ottesen@askcody.com>
2023-10-28 12:53:39 +0200

    Initial setup

---
+++ src/API/Controllers/defaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class defaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
